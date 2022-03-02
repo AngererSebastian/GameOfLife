5502e78e04e9dbf81b1e5177173393e5f346ba54
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2022-03-02 21:04:54 +0100

    display usage

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,21 @@
 module Main where
 
+import Data.Maybe
 import Draw
 import ParsePlain
 import System.Environment
 
+headMaybe (x : xs) = Just x
+headMaybe [] = Nothing
+
+usage = putStrLn "usage:\n\tgameOfLife <cellsFile>"
+
 main :: IO ()
-main = getArgs >>= (readFile . head) >>= (simulateBoard . parsePlain)
+--main = getArgs >>= (readFile . head) >>= (simulateBoard . parsePlain)
+main = do
+  args <- getArgs
+  let file = headMaybe args
+
+  case file of
+    Just f -> readFile f >>= (simulateBoard . parsePlain)
+    Nothing -> usage
